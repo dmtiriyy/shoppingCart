fc6c51acfc146eeaafad7f598354c339fc90e1a2
dmtiriyy <dim.evtushenckoyandex.ua@gmail.com>
2023-10-26 17:00:40 +0300

    react project

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/products/iph.jpeg";
+import product2 from "./assets/products/w.webp";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/lapt.jpg";
+import product5 from "./assets/products/tv.webp";
+import product6 from "./assets/products/pl.webp";
+import product7 from "./assets/products/pc.jpg";
+import product8 from "./assets/products/rob.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "IPhone",
+    price: 710.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "SmartWatch",
+    price: 360.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Cannon M50 Camera",
+    price: 699.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "HP Laptop",
+    price: 400.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "TV Philips",
+    price: 750.0,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Playstation 5",
+    price: 680.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "LDLC PC ART",
+    price: 920.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Samsung Robot Cleaner",
+    price: 250.0,
+    productImage: product8,
+  },
+];
